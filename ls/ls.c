0147e81d40b810037762e64ae8bc937927beb2fa
Sumith Shenoy <codernerd04@gmail.com>
2026-07-08 11:23:06 +0530

    ls implementation in C

---
+++ ls/ls.c
@@ -0,0 +1,38 @@
+/**
+ *
+ * ls command in C
+ *
+ **/
+
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+
+    /*
+           struct dirent {
+               ino_t          d_ino;       // Inode number 
+               off_t          d_off;       // Not an offset; see below 
+               unsigned short d_reclen;    // Length of this record 
+               unsigned char  d_type;      // Type of file; not supported
+                                           //  by all filesystem types 
+               char           d_name[256]; // Null-terminated filename 
+           };
+    */
+
+    char cwd[128];
+    getcwd(cwd, sizeof cwd);
+    printf("%s\n", cwd);
+
+    // open the dir
+    DIR* dir = opendir(cwd);
+
+    // read the dir
+    struct dirent *dir_entry;
+    while((dir_entry = readdir(dir)) != NULL) {
+        printf("%s\n", dir_entry->d_name);
+    }
+    
+    return 0;
+}
